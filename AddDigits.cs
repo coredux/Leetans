cdc17c50f88d888ebd8e92cdd6198425f4c2d6b6
coredux <redpath000@gmail.com>
2016-02-01 22:31:53 +0800

    add digits

--- AddDigits.cs
+++ AddDigits.cs
@@ -4,9 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Leetcode {
-    // leetcode258
+namespace LeetCode {
     class AddDigits {
+
+        public int doAddDigits( int num ) {
+
+            while( num >= 10 ) {
+                int ans = 0;
+                while( num != 0  ) {
+                    int tmp = num / 10;
+                    int lastDigit = num - tmp * 10;
+                    ans += lastDigit;
+                    num = tmp;
+                    
+                }
+                num = ans;
+                ans = 0;
+                
+            }
+            return num;
+        }
 
     }
 }
