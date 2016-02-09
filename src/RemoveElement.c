08a4f28953dca87c21a3b9d96103866b6205e2c8
coredux <redpath000@gmail.com>
2016-02-10 00:17:06 +0800

    27. Remove Element

---
+++ src/RemoveElement.c
@@ -0,0 +1,42 @@
+//
+// Created by seanr on 2016/2/10.
+//
+
+#include <stddef.h>
+#include <stdbool.h>
+
+// submit the codes below
+
+int removeElement(int* nums, int numsSize, int val) {
+
+    if( nums == NULL ) {
+        return 0;
+    }
+
+    for( int i = 0 ; i < numsSize ; i++ ) {
+        if( *( nums + i ) == val ) {
+            bool isFound = false;
+            int foundPlace = 0;
+            for( int j = i + 1 ; j < numsSize ; j++ ) {
+                if( *( nums + j ) != val ) {
+                    isFound = true;
+                    foundPlace =  j ;
+                }
+            }
+            if( !isFound ) {
+                return i;
+            }
+            *( nums + i ) = *( nums + foundPlace );
+            *( nums + foundPlace ) = val;
+        }
+    }
+    return numsSize;
+
+}
+
+// submit the codes above
+
+int main() {
+    return 0;
+}
+
