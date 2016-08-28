b5a964b39416790a812f8c3f71e57b6ca799e08c
Sean Ran <redpath000@gmail.com>
2016-08-28 10:36:05 +0800

    377. Combination Sum 4

---
+++ src/CombinationSum4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp( target + 1 , 0 );
+        dp[0] = 1;
+        for ( int i = 1; i <= target; ++i ) {
+            for ( auto j = nums.begin() ; j != nums.end(); ++j ) {
+                int nowNum = *j;
+                if ( i - nowNum >= 0 ) {
+                    dp[i] += dp[i-nowNum];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
